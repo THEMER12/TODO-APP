c413db1814e1cc05548d52920ed5a42aca0afab5
emerson <jackrojo21jk@gmail.com>
2025-07-10 20:27:39 +0200

    DbConnection update finished

---
+++ backend/DbConnection.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="todo_app_emer"
+        )
+        if connection.is_connected():
+            print("Successfully connected to MySQL database")
+            return connection
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
