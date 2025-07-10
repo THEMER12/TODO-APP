001b2c3e9ce5dfd7e711adb37d4936d02150da78
emerson <jackrojo21jk@gmail.com>
2025-07-10 11:04:12 +0200

    subir a github

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="flex w-screen h-screen">
+  <div className="w-1/2 h-screen  flex items-center justify-center">
+    <h1 className="text-9xl font-bold underline text-[#E3B6CB]">
+      TODO <br/> APP
+    </h1>
+  </div>
+  
+  <div className="w-1/2 h-screen bg-[#E3B6CB]">
+    
+  </div>
+</main>
+
+  )
+}
+
+export default App
