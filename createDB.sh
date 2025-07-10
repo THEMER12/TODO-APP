ff7351b075d311c59f0cec8faa4923036a273e9a
emerson <jackrojo21jk@gmail.com>
2025-07-10 11:41:58 +0200

    script for db

---
+++ createDB.sh
@@ -0,0 +1,3 @@
+echo "Creating database for the Todo app..."
+mysql -u root -p <todo_app.sql
+echo "Database created successfully!"
---
+++ todo_app.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS todo_app_emer;
+USE todo_app_emer;
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT FALSE
+)
